b2b97072311776b33e25a3bf77cbc14254628121
0xWarning <65122657+0xWarning@users.noreply.github.com>
2022-05-14 22:51:32 +0100

    Create avatar.js

---
+++ commands/tools/avatar.js
@@ -0,0 +1,27 @@
+module.exports = {
+   name:'avatar',
+   description: "Gets the avatar of a user or yourself",
+   usage: "[command | user] or [command]",
+   category: "tools",
+   requiredPermissions: ["ADMINISTRATOR", "KICK_MEMBERS"],
+   execute(message,args, cmd, client, Discord){
+      //command
+
+        /* If user isnt found it selects ur profile */
+        const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(x => x.user.username.toLowerCase() === args.slice(0).join(' ') || x.user.username === args[0]) || message.member;
+
+        if (!member.user.avatarURL) return message.channel.send(`That user does not have an avatar`);
+
+        const avatar = new Discord.MessageEmbed()
+			.setTitle(`${member.user.username}'s Avatar`)
+            .setColor("RANDOM")
+            .setImage(member.user.avatarURL())
+            .setColor(member.displayHexColor)
+            .setImage(member.user.displayAvatarURL({ dynamic: true, size: 2048 }))
+            .setURL(member.user.avatarURL())
+        message.channel.send(avatar)
+            // If bot doesnt have embed perms 
+            .catch(() => message.channel.send('**Error:** Missing permission `Embed link` '));
+   }
+
+}
